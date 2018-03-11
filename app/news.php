e6f93aafb741cb549ab47d76c075df841e34bc94
Manu <manohar.webdev@gmail.com>
2018-03-11 14:07:57 +0530

    Completed News Page

--- app/news.php
+++ app/news.php
@@ -15,8 +15,127 @@
 </section>
 
 <!-- News Posts -->
-<section class="news-posts">
-	
+<section class="news-posts main-bg">
+	<div class="container py-5">
+		<div class="text-center text-white">
+			<h3>Our News</h3>
+			<img src="assets/images/hr.png" class="img img-fluid py-4">
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card-columns text-white">
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_1.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Praesent sem dignisim</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_2.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Aenean eget mattis</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Curabitur nisi ultricies</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_3.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Nullam suscipit mas</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_4.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Fusce quis velit in erat molestie</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_5.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Invel tempor solli</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Vivamus eget arcu in leo</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_6.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Maecenas et imperdiet</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_7.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Suspendisse sed sagittis</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_8.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Etiam quis justo sed</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				  	<div class="card sec-bg">
+					    <img class="card-img-top" src="assets/images/news_9.jpg" alt="Card image cap">
+					    <div class="card-body">
+						    <h3 class="py-3 card-title">Praesent sem dignisim</h3>
+						    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem praesentium iste atque adipisci natus pariatur dicta inventore ratione sequi animi.</p>
+						    <div>
+			            		<button class="my-3 btn btn-outline-light slider--button">Read More</button>
+			            	</div>
+					    </div>
+				  	</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
 <?php require("include/footer.php"); ?>
